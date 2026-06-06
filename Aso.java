aa3a6ddfd523b65181115d83f7f97df136c3c533
chellomithasaskiabella <chellomitha61@gmail.com>
2026-06-06 18:49:06 +0700

    menambahkan getter info

--- Aso.java
+++ Aso.java
@@ -15,11 +15,17 @@ abstract class Orang {
 }
 
 class Matakuliah {
-    Private String kode;
-    Private String nama;
+    private String kode;
+    private String nama;
     private int sks;
 
-    public MataKuliah(String kode, String nama, int sks) {
-        
+    public Matakuliah (String kode, String nama, int sks) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+    public String getInfo() {
+        return kode + "-" + nama + "(" + sks + "SKS)";
     }
 }
